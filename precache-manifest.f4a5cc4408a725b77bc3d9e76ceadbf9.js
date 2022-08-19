a89c0c2807cbf4c3ffba465aad6afac252fcda92
Arthur Aguiar <artthurgaguiar@hotmail.com>
2022-08-19 00:57:58 -0300

    Updates

---
+++ precache-manifest.f4a5cc4408a725b77bc3d9e76ceadbf9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e47afa9e2bda5ad3f0d",
+    "url": "/KantoDex/static/css/main.fb006d0a.chunk.css"
+  },
+  {
+    "revision": "4e47afa9e2bda5ad3f0d",
+    "url": "/KantoDex/static/js/main.4e47afa9.chunk.js"
+  },
+  {
+    "revision": "d11720e887a1448ed361",
+    "url": "/KantoDex/static/js/1.d11720e8.chunk.js"
+  },
+  {
+    "revision": "02ff2822337b1d488ab0",
+    "url": "/KantoDex/static/js/2.02ff2822.chunk.js"
+  },
+  {
+    "revision": "9e1ec5c57075a714cc29",
+    "url": "/KantoDex/static/js/runtime~main.9e1ec5c5.js"
+  },
+  {
+    "revision": "3b50df2691671da6744dac08c52ae679",
+    "url": "/KantoDex/static/media/pokedex.3b50df26.png"
+  },
+  {
+    "revision": "5a123aac93da9121ee581e72e7c5b081",
+    "url": "/KantoDex/static/media/hoverSound.5a123aac.mp3"
+  },
+  {
+    "revision": "9ddc898dfe90b49a420d64e429e09391",
+    "url": "/KantoDex/static/media/clickSound.9ddc898d.mp3"
+  },
+  {
+    "revision": "a756d145935852b09a0ad8e3e986e8be",
+    "url": "/KantoDex/static/media/home.a756d145.png"
+  },
+  {
+    "revision": "4542575f45e165e37d0e943b48566264",
+    "url": "/KantoDex/static/media/bg-dark.4542575f.PNG"
+  },
+  {
+    "revision": "cfb3681487204b7fa450d202947d79b7",
+    "url": "/KantoDex/static/media/bg.cfb36814.png"
+  },
+  {
+    "revision": "8e723033a7a5934fdad3a68a00bd13e5",
+    "url": "/KantoDex/index.html"
+  }
+];
